0c9a291d17062dfb68f87b8fbf167852aacd0bc9
Emloop12 <emloop.cl@gmail.com>
2025-02-10 22:20:27 -0600

    Update Ejercicio1.cs

--- Ejercicio1.cs
+++ Ejercicio1.cs
@@ -3,14 +3,16 @@ using System;
 class Ejercicio1 {
     static void Main() {
         double[] calificaciones = new double[6];
+        double suma = 0;
         
         for (int i = 0; i < 5; i++) {
             Console.Write($"Ingrese la calificación {i + 1}: ");
             calificaciones[i] = double.Parse(Console.ReadLine());
+            suma += calificaciones[i];
         }
 
         // Calcular el promedio
-        calificaciones[5] = (calificaciones[0] + calificaciones[1] + calificaciones[2] + calificaciones[3] + calificaciones[4]) / 5;
+        calificaciones[5] = suma / 5;
 
         // Mostrar calificaciones y promedio
         Console.WriteLine("\nCalificaciones ingresadas:");
